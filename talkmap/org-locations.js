259067f2fdb3d90a8138cfe7b45c9bdbfe07342f
Thpreis <thimo.preis@posteo.de>
2020-04-09 15:37:03 +0200

    Revamped talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Heidelberg, Germany",
+    49.4093582,
+    8.694724
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Durham, UK",
+    54.666667,
+    -1.75
   ]
 ];
